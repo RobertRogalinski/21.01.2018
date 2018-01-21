f372ec75ad83e3b4d9d25eee881fdcb5644ce27e
Robert Rogaliński <rrogalinski@linux-p110-15.inf.ug.edu.pl>
2018-01-21 12:44:34 +0100

    zad4

---
+++ zad4/zad4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+  int a,b;
+  {scanf("%d %d" &a, &b );
+int wynik=a*b;
+printf("wynik a*b= %d\n", wynik );
+}
+  return 0;
+}
